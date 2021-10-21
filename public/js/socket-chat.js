88d552f719b5d6d1af1e13a21e9b6f3e41b2c24d
Manuel <mninahuanca4@gmail.com>
2021-10-21 17:48:38 -0400

    Enviando mensajes a todo el grupo

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -22,18 +22,24 @@ socket.on("disconnect", function () {
 });
 
 // Enviar información
-socket.emit(
-  "enviarMensaje",
-  {
-    usuario: "Fernando",
-    mensaje: "Hola Mundo",
-  },
-  function (resp) {
-    console.log("respuesta server: ", resp);
-  }
-);
+// socket.emit(
+//   "enviarMensaje",
+//   {
+//     usuario: "Fernando",
+//     mensaje: "Hola Mundo",
+//   },
+//   function (resp) {
+//     console.log("respuesta server: ", resp);
+//   }
+// );
 
 // Escuchar información
 socket.on("crearMensaje", function (mensaje) {
   console.log("Servidor:", mensaje);
+});
+
+//Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on("listaPersona", function (personas) {
+  console.log(personas);
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,5 +1,7 @@
 const { Usuarios } = require("../classes/usuarios");
 const { io } = require("../server");
+const { crearMensaje } = require("../utils/utilidades");
+
 const usuarios = new Usuarios();
 
 io.on("connection", (client) => {
@@ -11,14 +13,24 @@ io.on("connection", (client) => {
       });
     }
     let personas = usuarios.agregarPersona(client.id, data.nombre);
+
+    client.broadcast.emit("listaPersona", usuarios.getPersonas());
     return callback(personas);
+  });
+
+  client.on("crearMensaje", (data) => {
+    let persona = usuarios.getPersonas(client.id);
+    let mensaje = crearMensaje(persona.nombre, data.mensaje);
+    client.broadcast.emit("crearMensaje", mensaje);
   });
 
   client.on("disconnect", () => {
     let personaBorrada = usuarios.borrarPersona(client.id);
-    client.broadcast.emit("crearMensaje", {
-      usuario: "Administrador",
-      mensaje: `${personaBorrada} abandono el chat`,
-    });
+    client.broadcast.emit(
+      "crearMensaje",
+      crearMensaje("Administrador", `${personaBorrada.nombre} salió`)
+    );
+
+    client.broadcast.emit("listaPersona", usuarios.getPersonas());
   });
 });
